1a00a70537170318c14fb649d477c2046915a084
CHANDAN RAJ <69450631+chandan95raj@users.noreply.github.com>
2021-01-25 07:51:27 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,25 +1,8 @@
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-  document.getElementById("cntb").style.backgroundColor = "white";
-
-  
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-  document.getElementById("cntb").style.backgroundColor = "";
-}
-
-function t(){
-
-    var alldate = new Date();
-    var tim = alldate.toLocaleTimeString();
-    var day = alldate.getDate();
-    var month = alldate.getMonth()+1;
-    var year = alldate.getFullYear();
-    var ti = document.getElementById("time");
-    ti.innerHTML = day+ "/" + month + "/" + year + " . " +tim+ " ";
-}
-
-
-setInterval(t,100);
